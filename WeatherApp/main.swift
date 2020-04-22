44e34563109930e6852b212a64b036d03af5c1d0
Francis Murray <franciswmurray@gmail.com>
2020-04-22 15:23:30 +0200

    Initial Commit

---
+++ WeatherApp/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  WeatherApp
+//
+//  Created by Francis Murray on 4/22/20.
+//  Copyright © 2020 Francis Murray & Massyl Djemai. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
