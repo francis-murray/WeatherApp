bf9674f26bedc18a8548b518769557c079dcb591
Francis Murray <franciswmurray@gmail.com>
2020-05-01 16:16:26 +0200

    improve getCitiesFromUser() user experience

--- WeatherApp/Services/CityServices.swift
+++ WeatherApp/Services/CityServices.swift
@@ -39,13 +39,13 @@ class CityServices {
         var cityArray: [City] = []
         var userInput: String
         repeat {
-            print("Please enter a city name: ")
+            print("Please add a city name or enter 'q' when your done: ")
             userInput = readLine()!
-            if !userInput.isEmpty {
+            if !userInput.isEmpty && userInput != "q" {
                 if let city = cityServices.findCity(cityName: userInput, jsonFileURL: Constants.jsonFileURL) {
                     print("You have entered \(city.name), \(city.country)")
                     cityArray.append(city)
-                    print("List of cities: ", terminator: "")
+                    print("Current list of cities: ", terminator: "")
                     for city in cityArray {
                         print("\(city.name), \(city.country)", terminator: "; ")
                     }
@@ -53,11 +53,8 @@ class CityServices {
                 } else {
                     print("The city \(userInput) you entered could not be found")
                 }
-
-                print("Enter another city? (y/n)")
-                userInput = readLine()!
             }
-        } while(userInput != "n")
+        } while(userInput != "q")
         return cityArray
     }
     
